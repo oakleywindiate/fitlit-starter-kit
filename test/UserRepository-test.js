657481c4915ff8f08c821d07f2954a4cb81b577e
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-02-05 12:45:11 -0600

    Add test for UserRepo class to be a function

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,18 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import userData from '../src/data/users.js';
 
 describe('User Repository', () => {
+
+  let userRepo;
+
+  beforeEach(() => {
+      userRepo = new UserRepository(userData)
+
+    });
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
+
 });
