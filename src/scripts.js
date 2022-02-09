09fca2c588c75cbce923978922241b764099eaa6
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-02-08 19:56:35 -0600

    Add greeting display functionality to dom

--- src/scripts.js
+++ src/scripts.js
@@ -21,18 +21,29 @@ import User from './User';
 const userName = document.querySelector('#userName')
 const userAddress = document.querySelector('#userAddress')
 const userEmail = document.querySelector('#userEmail')
-
+const welcomeBanner = document.querySelector('#welcomeBanner')
 
 
 let userRepo = new UserRepository(userData)
 let user1 = new User(userRepo.identifyUser(1))
-console.log(user1)
+console.log(user1.returnUserFirstName())
+console.log(userRepo.averageStepGoal())
+
+const stepComparison = () => {
+
+  let average = userRepo.averageStepGoal()
+  console.log(user1.dailyStepGoal - average)
+  return user1.dailyStepGoal - average
+}
+// console.log(stepComparison())
 
 const displayUser = () => {
  userName.innerText = user1.name
  userAddress.innerText = user1.address
  userEmail.innerText = user1.email
- 
+ // welcomeBanner.innerText = user1.returnUserFirstName()
+ stepComparison()
   return
 }
+
 window.addEventListener('load', displayUser)
