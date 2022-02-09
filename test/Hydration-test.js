61e93b22538e37ddf521f2716101e81f0db22727
Igor Decess <i.decess@comcast.net>
2022-02-08 19:21:01 -0700

    Add future methods for hydration test file

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -4,8 +4,13 @@ import Hydration from '../src/Hydration';
 describe('Hydration', () => {
 
   let hydration;
+  let hydrationData;
 
   beforeEach(() => {
+
+    hydrationData = [
+      {userID: 1}, {ouncesConsumed: 3}
+    ];
 
     hydration = new Hydration();
   });
@@ -19,5 +24,17 @@ describe('Hydration', () => {
   });
 
 
+//IN PROGRESS
+  it('should to able to calculate daily average in ounces', () => {
+    expect(null.drinkDailyAverage()).to.equal("");
+  });
+
+  it('should to able to show daily amount consumed', () => {
+    expect(null.drinkDailyAmount()).to.equal("");
+  });
+
+  it('should to able show seven day average', () => {
+    expect(null.drinkSevenDayAve()).to.equal("");
+  });
 
 });
