cd16008ec7dbfb8618d668c59f87567b40fc2f89
Oakley Windiate <oakley.windiate@gmail.com>
2022-02-10 15:33:54 -0700

    Add API calls for sleep, hydration, activity, and user

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,20 @@
 // Your fetch requests will live here!
 
+fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+  .then(response => response.json())
+  .then(data => console.log(data))
+
 
 console.log('I will be a fetch request!')
-
