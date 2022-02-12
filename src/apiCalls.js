4e9c36312b7b3c65b40d3e53751fb52881d36d36
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-02-11 20:21:03 -0600

    Change promise structure and fetch export

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,60 +1,11 @@
 // Your fetch requests will live here!
 // import UserRepository from './UserRepository';
 
-export const userData = fetch("https://fitlit-api.herokuapp.com/api/v1/users");
-export const sleepData = fetch("https://fitlit-api.herokuapp.com/api/v1/sleep");
-export const activityData = fetch("https://fitlit-api.herokuapp.com/api/v1/activity");
-export const hydrationData = fetch("https://fitlit-api.herokuapp.com/api/v1/hydration");
-  // console.log(userData)
-  // .then(response => response.json())
-  // .then(data => console.log(data))
-Promise.all([userData, sleepData, activityData, hydrationData])
-
-.then(data => {
-  data.forEach(response => {
-    process(response.json());
-  })
-})
-
-const process = (prom => {
-  const result = prom.then(data => {
-    console.log(result)
-    sendData(data);
-    return data;
-  })
-  // console.log(result);
-
-// })
-// process();
-//
-// function sendData(para) {
-//   let userRepo = new UserRepository(para.userData)
-//   console.log(userRepo)
-//   // return userRepo
-// }
-
-
-
-
-
-
-
-
-
-// fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//
-// fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//
-// fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-
-
-
-console.log('I will be a fetch request!')
-
-// export default data;
+const userData = fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+.then(response => response.json())
+const sleepData = fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+.then(response => response.json())
+// export const activityData = fetch("https://fitlit-api.herokuapp.com/api/v1/activity");
+const hydrationData = fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+.then(response => response.json())
+export {userData, sleepData, hydrationData}
