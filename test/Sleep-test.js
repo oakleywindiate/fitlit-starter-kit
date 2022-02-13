59ef515cf1ec4ad34e2732f902f4b37938e0c33b
Igor Decess <i.decess@comcast.net>
2022-02-13 00:40:18 -0700

    Add sleep data to sleep test file

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -3,11 +3,56 @@ import Sleep from '../src/Sleep';
 
 describe('Sleep', () => {
 
-  let sleep;
+  let sleep1;
+  let sleepData;
 
   beforeEach(() => {
 
-    sleep = new Sleep();
+    sleepData = [
+    {
+      "userID": 1,
+      "date": '2022/02/01',
+      "hoursSlept": 6,
+      "sleepQuality": 9,
+    },
+    {
+      "userID": 1,
+      "date": '2022/02/02',
+      "hoursSlept": 7,
+      "sleepQuality": 10,
+    },
+    {
+      "userID": 1,
+      "date": '2022/02/03',
+      "hoursSlept": 5,
+      "sleepQuality": 4,
+    },
+    {
+      "userID": 1,
+      "date": '2022/02/04',
+      "hoursSlept": 6,
+      "sleepQuality": 6,
+    },
+    {
+      "userID": 1,
+      "date": '2022/02/05',
+      "hoursSlept": 9,
+      "sleepQuality": 10,
+    },
+    {
+      "userID": 1,
+      "date": '2022/02/06',
+      "hoursSlept": 7,
+      "sleepQuality": 5,
+    },
+    {
+      "userID": 1,
+      "date": '2022/02/07',
+      "hoursSlept": 8,
+      "sleepQuality": 9,
+    }];
+
+    sleep1 = new Sleep(1, sleepData);
   });
 
   it('should be a function', () => {
