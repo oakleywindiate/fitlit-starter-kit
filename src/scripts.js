d8e6ef010de55163084bdf09e0e9f47fcda4c37b
Oakley Windiate <oakley.windiate@gmail.com>
2022-02-14 16:52:41 -0700

    Refactor scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,8 @@
 // --------- IMPORTS ---------
+
 import './css/styles.css';
 import { userData, sleepData, hydrationData, activityData } from './apiCalls.js'
 import './images/turing-logo.png'
-import './images/profile-svgrepo-com.svg'
-// import './images/sleep.svg'
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 import UserRepository from './UserRepository';
@@ -13,6 +12,7 @@ import Sleep from './Sleep';
 import {hydrationChart, stepChart, sleepChart, foodChart} from './charts.js'
 
 // --------- QUERY SELECTORS/VARIABLES ---------
+
 const userName = document.querySelector('#userName')
 const userAddress = document.querySelector('#userAddress')
 const userEmail = document.querySelector('#userEmail')
@@ -25,26 +25,16 @@ const allTimeQuality = document.querySelector('#allTimeQuality')
 const waterWeek = document.querySelector('#waterWeek')
 
 // --------- FUNCTIONS ---------
+
 const onLoad = () => {
   Promise.all([userData, sleepData, hydrationData, activityData])
   .then(data => manageData(data) )
 }
 
-const makeCharts = (data, h2oAvg, h2oToday, dailySleep, sleepQuality) => {
-  const ctx = document.getElementById('myChart1').getContext('2d');
-  const ctx2 = document.getElementById('myChart2').getContext('2d');
-  const ctx3 = document.getElementById('myChart3').getContext('2d');
-  const ctx4 = document.getElementById('myChart4').getContext('2d');
-  hydrationChart(ctx, h2oAvg, h2oToday)
-  stepChart(ctx2, data[3])
-  sleepChart(ctx3, dailySleep, sleepQuality)
-  foodChart(ctx4, 'banana')
-}
-
 const manageData = (data) => {
   let users = data[0].userData.map(user => new User(user))
   let userRepo = new UserRepository(users)
-  const user1 = userRepo.userData[getRandomIndex(userRepo.userData)]
+  let user1 = userRepo.userData[getRandomIndex(userRepo.userData)]
   let hydroData = new Hydration(data[2].hydrationData)
   let h2oAvg = hydroData.drinkDailyAverage(user1.id)
   let sleepData = new Sleep(data[1].sleepData)
@@ -56,15 +46,23 @@ const manageData = (data) => {
   let sleepQualityAverage = sleepData.sleepQualityAverage(user1.id)
   let h2oToday = hydroData.drinkDailyAmount(user1.id)
   let h2oSevenDay = hydroData.drinkSevenDaysData(user1.id)
-  console.log('sleep', )
-  console.log()
 
   makeCharts(data, h2oAvg, h2oToday, dailySleep, sleepQuality,)
   displayUser(userRepo, user1, h2oSevenDay, sleepSevenDay, sleepQualityWeek, sleepQualityAverage, sleepDailyAverage)
 }
 
+const makeCharts = (data, h2oAvg, h2oToday, dailySleep, sleepQuality) => {
+  const ctx = document.getElementById('myChart1').getContext('2d');
+  const ctx2 = document.getElementById('myChart2').getContext('2d');
+  const ctx3 = document.getElementById('myChart3').getContext('2d');
+  const ctx4 = document.getElementById('myChart4').getContext('2d');
+  hydrationChart(ctx, h2oAvg, h2oToday)
+  stepChart(ctx2, data[3])
+  sleepChart(ctx3, dailySleep, sleepQuality)
+  foodChart(ctx4)
+}
+
 const displayUser = (userRepo, user1, h2oSevenDay, sleepSevenDay, sleepQualityWeek, sleepQualityAverage, sleepDailyAverage) => {
-  // console.log(user1.id)
   let name = user1.returnUserFirstName()
   welcomeBanner.innerText = `Welcome, ${name}!`
   userName.innerText = user1.name
@@ -76,7 +74,6 @@ const displayUser = (userRepo, user1, h2oSevenDay, sleepSevenDay, sleepQualityWe
   allTimeQuality.innerText = `All Time Average Sleep Quality: ${sleepQualityAverage}`
   allTimeHours.innerText = `All Time Average Sleep Hours: ${sleepDailyAverage}`
   waterWeek.innerText = `Water Consumed Per Day This Week: ${h2oSevenDay}`
-  return
 }
 
 function getRandomIndex(array) {
