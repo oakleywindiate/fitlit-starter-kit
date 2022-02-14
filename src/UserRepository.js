37f7508cdf8309e97b1007652645a6bbc56add6e
Igor Decess <i.decess@comcast.net>
2022-02-13 17:11:25 -0700

    Refactor UserRepository class to function properly

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -3,22 +3,18 @@ class UserRepository {
   constructor(users) {
     this.userData = users;
   }
+
   identifyUser(id) {
-    return this.userData.find((user) => user.id === id)
+    return this.userData.find(user => user.id === id);
   }
+
   averageStepGoal() {
     const stepGoals = this.userData.map(steps => {
-      return steps.dailyStepGoal
-    })
+      return steps.dailyStepGoal;
+    });
     const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    return ((stepGoals.reduce(reducer)) / stepGoals.length);
+    return Math.round((stepGoals.reduce(reducer)) / stepGoals.length);
   }
-  // stepComparison() {
-  //   let average = userRepo.averageStepGoal()
-  //   let result = (user1.dailyStepGoal - average) / user1.dailyStepGoal
-  //   return `Your daily step goal is ${result} above the average user!`
-  // }
 }
-
 
 export default UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,14 +1,33 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
-import userData from '../src/data/users.js';
 
 describe('User Repository', () => {
 
   let userRepo;
+  let userData;
 
   beforeEach(() => {
-      userRepo = new UserRepository(userData)
+    userData = [
+    {
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [16, 4, 8]
+    },
+    {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [9, 18, 24, 19]
+    }],
 
+    userRepo = new UserRepository(userData);
     });
 
   it('should be a function', () => {
@@ -30,16 +49,11 @@ describe('User Repository', () => {
     "email": "Dimitri.Bechtelar11@gmail.com",
     "strideLength": 4.5,
     "dailyStepGoal": 5000,
-    "friends": [
-      9,
-      18,
-      24,
-      19
-    ]})
+    "friends": [9, 18, 24, 19]
+    });
   });
 
   it('should find the average step goal amongst users', () => {
-    expect(userRepo.averageStepGoal()).to.equal(6700);
+    expect(userRepo.averageStepGoal()).to.equal(7500);
   });
-
 });
