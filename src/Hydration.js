00c8b5a51631c26d0abe6a212e8e8cc96a308dca
Igor Decess <i.decess@comcast.net>
2022-02-22 15:46:18 -0700

    Refactor hydration class method naming

--- src/Hydration.js
+++ src/Hydration.js
@@ -3,21 +3,21 @@ class Hydration {
     this.hydrationData = generalHydroData;
   }
 
-  drinkDailyAverage(id) {
+  calculateDailyDrinkAverage(id) {
     const filteredHydroData = this.hydrationData.filter(data => data.userID === id);
     const mappedData = filteredHydroData.map(data => data.numOunces);
     const ounces = (previousValue, currentValue) => previousValue + currentValue;
     return ((mappedData.reduce(ounces)) / mappedData.length);
   }
 
-  drinkDailyAmount(id) {
+  showDailyDrinkAmount(id) {
     const filterOneUser = this.hydrationData.filter(data => data.userID === id);
     const findIndex = filterOneUser.length - 1;
     const findOuncesDrank = filterOneUser[findIndex].numOunces;
     return findOuncesDrank;
   }
 
-  drinkSevenDaysData(id) {
+  calculate7DaysDrinkAverage(id) {
     const targetID = this.hydrationData.filter(generalHydroData => generalHydroData.userID === id);
     const targetIndex = targetID.length - 7;
     const returnSevenDays = targetID.slice(targetIndex);
