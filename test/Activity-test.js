a4a3e3b2b470fa8fe0137eda4d38c6ae6d1de581
Oakley Windiate <oakley.windiate@gmail.com>
2022-02-23 15:31:45 -0700

    Create user data for test

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,12 +1,47 @@
 import { expect } from 'chai';
 import Activity from '../src/Activity';
+import UserRepository from '../src/UserRepository';
 
 describe('Activity', () => {
 
   let activity1;
   let activityData;
+  let userData;
+
+  let currentUser =
+  let currentRepo = new UserRepository(users);
 
   beforeEach(() => {
+
+  userData = [
+    {
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [16,4,8]
+      },
+      {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [9,18,24,19]
+      },
+      {
+      "id": 3,
+      "name": "Herminia Witting",
+      "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+      "email": "Elwin.Tromp@yahoo.com",
+      "strideLength": 4.4,
+      "dailyStepGoal": 5000,
+      "friends": [19,11,42,33]
+    ]
+  }
 
   activityData = [
   {
@@ -84,7 +119,11 @@ describe('Activity', () => {
     expect(activity1).to.be.an.instanceof(Activity);
   });
 
+  it('should be able to calculate the miles a user has walked based on their steps for a specific day', () => {
+    expect().to.equal();
+  });
 
-
-  
+  it('should be able to calculate the total number of minutes a user was active on a specific date', () => {
+    expect(hydration1.calculateDailyDrinkAvg(1)).to.equal(81.25);
+  });
 });
