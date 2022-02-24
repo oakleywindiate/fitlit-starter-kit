e7b1a9b077e2593fa4b743fa2ba45e64ac388be4
Oakley Windiate <oakley.windiate@gmail.com>
2022-02-23 18:02:12 -0700

    Add calculate weekly average method

--- src/Activity.js
+++ src/Activity.js
@@ -10,8 +10,16 @@ class Activity {
   showMinutesActive(user) {
     const findUser = this.activityData.filter(data => data.userID === user.id)
     const findIndex = findUser.length - 1;
-    console.log(findUser[findIndex].minutesActive)
     return findUser[findIndex].minutesActive;
+  }
+  calculateAvgWeeklyMinActive(user) {
+    const findUser = this.activityData.filter(data => data.userID === user.id)
+    const findIndex = findUser.length - 7;
+    const findSevenDays = findUser.slice(findIndex)
+    const reduceDays = findSevenDays.reduce((acc, day) => {
+      return acc += day.minutesActive
+    }, 0)
+    return (reduceDays / 7).toFixed(2);
   }
 }
 
