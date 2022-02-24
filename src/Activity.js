72f078b88a52e56a19dee3133885503ab752c501
Oakley Windiate <oakley.windiate@gmail.com>
2022-02-23 17:34:31 -0700

    Create method for miles and minutes active

--- src/Activity.js
+++ src/Activity.js
@@ -2,8 +2,18 @@ class Activity {
   constructor(generalActivityData) {
     this.activityData = generalActivityData;
   }
-
-
+  calculateMiles(user) {
+    const findUser = this.activityData.filter(data => data.userID === user.id)
+    const findIndex = findUser.length - 1;
+    return ((findUser[findIndex].numSteps * user.strideLength) / 5280).toFixed(2);
+  }
+  showMinutesActive(user) {
+    const findUser = this.activityData.filter(data => data.userID === user.id)
+    const findIndex = findUser.length - 1;
+    console.log(findUser[findIndex].minutesActive)
+    return findUser[findIndex].minutesActive;
+  }
 }
+
 
 export default Activity;
