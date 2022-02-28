8ef0ee2223e740b7dc430d4dfe91b55f7c86d9d6
Igor Decess <i.decess@comcast.net>
2022-02-28 14:06:47 -0700

    Add ticker zero to display some fun info

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,6 +9,7 @@ const userStepAverage = document.querySelector('#userStepAverage')
 const sleepWeek = document.querySelector('#sleepWeek')
 const sleepAvg = document.querySelector('#sleepAvg')
 const ticker = document.querySelector('#ticker')
+const ticker0 = document.querySelector('#tickerItem0')
 const ticker1 = document.querySelector('#tickerItem1')
 const ticker2 = document.querySelector('#tickerItem2')
 const ticker3 = document.querySelector('#tickerItem3')
@@ -17,6 +18,7 @@ const ticker5 = document.querySelector('#tickerItem5')
 const ticker6 = document.querySelector('#tickerItem6')
 
 // --------- FUNCTIONS ---------
+
 const displayHydroData = (h2oSevenDay) => {
   waterWeek.innerText = `Water Consumed Per Day This Week: ${h2oSevenDay}`
 }
@@ -31,6 +33,7 @@ const displayUserData = (userRepo, user1) => {
 }
 
 const displaySleepTicker = (sleepSevenDay, sleepQualityWeek) => {
+  ticker0.innerText = 'Boarding for data info begins here!!!'
   ticker1.innerText = `7 day sleep hours by day = ${sleepSevenDay}`
   ticker2.innerText = `7 day sleep hours by day = ${sleepQualityWeek}`
 }
@@ -42,7 +45,7 @@ const displayHydroTicker = (h2oSevenDay) => {
 const displayActivityTicker = (dailyMiles, avgUserSteps, avgUserMinActive, avgUserStairsClimbed) => {
 ticker4.innerText = `You walked ${dailyMiles} miles today!`
 ticker5.innerText = `The average user took ${avgUserSteps} steps, climbed ${avgUserStairsClimbed} flights and was active for ${avgUserMinActive} mins today.`
-ticker6.innerText = '🚃 choo - choo!!!'
+ticker6.innerText = '🚃 Choo - Choo!!!'
 }
 
 export {displayHydroData, displayUserData, displaySleepTicker, displayHydroTicker, displayActivityTicker};
